dec39e25af1585a78a23d376a315a9db16c64c8e
priyashi Khandka <priyashi.khandka2020@vitstudent.ac.in>
2023-06-19 16:55:54 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const USERNAME= process.env.DB_USERNAME;
 const PASSWORD= process.env.DB_PASSWORD;
 
 
-const URL=  `mongodb://${USERNAME}:${PASSWORD}@ac-2yol1ta-shard-00-00.fzwvsgs.mongodb.net:27017,ac-2yol1ta-shard-00-01.fzwvsgs.mongodb.net:27017,ac-2yol1ta-shard-00-02.fzwvsgs.mongodb.net:27017/PROJECT0?ssl=true&replicaSet=atlas-b2odxl-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const URL=  process.env.MONGODB_URI || `mongodb://${USERNAME}:${PASSWORD}@ac-2yol1ta-shard-00-00.fzwvsgs.mongodb.net:27017,ac-2yol1ta-shard-00-01.fzwvsgs.mongodb.net:27017,ac-2yol1ta-shard-00-02.fzwvsgs.mongodb.net:27017/PROJECT0?ssl=true&replicaSet=atlas-b2odxl-shard-0&authSource=admin&retryWrites=true&w=majority`;
    
 
 Connection(URL);
